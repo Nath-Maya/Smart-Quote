e470fee33d77d5ba8b4ddb6e6c75dee77d74bcf2
Nath-Maya <natha.maya.ramirez93@gmail.com>
2025-07-02 22:24:53 -0500

    Setup initial UI layout using GridLayoutManager with input and output areas

---
+++ src/main/java/org/example/MainForm.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JButton procesarButton;
+    private JButton copiarButton;
+}
